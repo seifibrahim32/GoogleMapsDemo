5986add81bb676ab823f21c01e610f031289e8db
Seif Ashraf <seifibrahim32@gmail.com>
2023-01-28 18:50:42 +0200

    Update

--- lib/features/presentation/bloc/bloc.dart
+++ lib/features/presentation/bloc/bloc.dart
@@ -5,22 +5,20 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import '../../data/data_sources/data_source.dart';
 import '../../data/models/maps_model.dart';
 import 'bloc_states.dart';
 
 class MapsCubit extends Cubit<MapStates> {
-
-  MapsCubit() :super(MapLoading());
+  MapsCubit() : super(MapLoading());
 
   static late Position position;
 
   static late final CameraPosition cameraPosition;
-  final Set<Marker> markers = { };
+  final Set<Marker> markers = {};
+
   Future<MapsModel?> getLocation() async {
     position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high
-    );
+        desiredAccuracy: LocationAccuracy.high);
     cameraPosition = CameraPosition(
       target: LatLng(position.latitude, position.longitude),
       zoom: 14.4746,
@@ -38,12 +36,11 @@ class MapsCubit extends Cubit<MapStates> {
 
   connectInternet() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile
-        || connectivityResult == ConnectivityResult.wifi) {
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
       log(connectivityResult.name);
       getLocation();
-    }
-    else {
+    } else {
       emit(MapLoading());
     }
   }
